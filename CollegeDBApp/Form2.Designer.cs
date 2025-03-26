dd9f8ec66a8a0df6936462ba4045a1d7acc4a156
Jomar120 <diazjomar2000@gmail.com>
2025-03-26 22:16:39 +0800

    Changing background color

--- CollegeDBApp/Form2.Designer.cs
+++ CollegeDBApp/Form2.Designer.cs
@@ -191,7 +191,7 @@ namespace CollegeDBApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.BackColor = System.Drawing.Color.OldLace;
             this.ClientSize = new System.Drawing.Size(637, 693);
             this.Controls.Add(this.txtDepartmentID);
             this.Controls.Add(this.label4);
